a14ef53ffa445fb7ca46b7800845d0c16e8e9695
Bruno Martins <bgcngm@gmail.com>
2026-05-30 12:55:05 +0100

    xueing: De-blob oplus interfaces
    Change-Id: I85d123e8c9bf8750b301cca3e62f7307071cc190

--- xueying-vendor.mk
+++ xueying-vendor.mk
@@ -1441,9 +1441,6 @@ PRODUCT_PACKAGES += \
     libthreadutils \
     libubifocus \
     libvideoml \
-    vendor.oplus.hardware.cameraextension-V1-ndk \
-    vendor.oplus.hardware.cammidasservice-V1-ndk_vendor \
-    vendor.oplus.hardware.commondcs-V1-ndk_platform \
     vendor.qti.hardware.camera.aon-service-impl \
     vendor.qti.hardware.camera.postproc@1.0-service-impl \
     com.qti.sensor.imx581 \
@@ -1644,13 +1641,9 @@ PRODUCT_PACKAGES += \
     libyuv2 \
     libyuvwrapper \
     libziparchive_odm \
-    vendor.oplus.hardware.camera_rfi-V1-ndk \
     vendor.oplus.hardware.camera_rfi-V1-service-impl \
     vendor.oplus.hardware.cameraextension-V1-service-impl \
-    vendor.oplus.hardware.cammidasservice-V1-ndk \
-    vendor.oplus.hardware.secure_element-V1-ndk \
     vendor.oplus.hardware.sendextcamcmd-V1-service-impl \
-    vendor.oplus.hardware.sendextcamcmd-V2-ndk \
     vendor.zeku.hardware.explorer@1.0 \
     odm_lib_rfsa_adsp_libtfadsp_sb5_0_rx_so \
     odm_lib_rfsa_adsp_libtfadsp_sb5_0_tx_so \
